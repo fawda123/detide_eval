1b87d40b782f59bb94a98bf6f12ab4efd2012af9
fawda123 <mbafs2012@gmail.com>
2020-10-05 09:23:32 -0400

    optim function working for optimal window widths

--- R/optimal_windows.R
+++ R/optimal_windows.R
@@ -1,51 +1,72 @@
 library(tidyverse)
 library(lubridate)
-library(WtRegDO)
+# library(WtRegDO)
+library(foreach)
+library(doParallel)
+devtools::load_all('../WtRegDO/')
 
-# dat <- read.csv('data/raw/APNERR2020.csv') %>% 
-#   mutate(
-#     DateTimeStamp = mdy_hm(DateTimeStamp, tz = 'America/Jamaica'), 
-#     mo = month(DateTimeStamp), 
-#     hr = hour(DateTimeStamp)
-#   )
-# 
-# flnm <- 'APNERR2020.csv'
-# tz <- 'America/Jamaica'
-# lat <- 29.75
-# long <- -85
-# dy <- 12
-# hr <- 6
-# td <- 0.5
-# 
-# # data
-# dat <- read.csv('data/raw/', paste0(flnm, '.csv')) %>% 
-#   mutate(DateTimeStamp = mdy_hm(DateTimeStamp, tz = tz)) %>% 
-#   na.omit %>% 
-#   unique
-# 
-# # weighted regression, optimal window widths for SAPDC from the paper
-# wtreg_res <- wtreg(dat, parallel = F, wins = list(dy, hr, td), progress = F, 
-#                    tz = tz, lat = lat, long = long)
-# 
-# # estimate ecosystem metabolism using observed DO time series
-# metab_obs <- ecometab(wtreg_res, DO_var = 'DO_obs', tz = tz, 
-#                       lat = lat, long = long)
-# 
-# # estimate ecosystem metabolism using detided DO time series
-# metab_dtd <- ecometab(wtreg_res, DO_var = 'DO_nrm', tz = tz, 
-#                       lat = lat, long = long)
+ncores <- detectCores()  
+cl <- makeCluster(ncores)
+registerDoParallel(cl)
 
+flnm <- 'APNERR2020'
+tz <- 'America/Jamaica'
+lat <- 29.75
+long <- -85
+dy <- 12
+hr <- 6
+td <- 0.5
 
-toplo <- mins %>% 
-  filter(obj == 'minano') %>% 
-  filter(nm == 'SAPDC') %>% 
-  mutate(est = pmax(350, est))
+# data
+dat <- read.csv(paste0('data/raw/', flnm, '.csv')) %>%
+  mutate(DateTimeStamp = mdy_hm(DateTimeStamp, tz = tz)) %>%
+  na.omit %>%
+  unique
 
-ggplot(toplo, aes(x = factor(dy), y = factor(hr), fill = est)) + 
-  geom_tile() + 
-  scale_x_discrete(expand = c(0, 0)) + 
-  scale_y_discrete(expand = c(0, 0)) + 
-  facet_grid(nm~td)
+# weighted regression, optimal window widths for SAPDC from the paper
+wtreg_res <- wtreg(dat, wins = list(12, 6, 0.5), progress = T, parallel = T,
+                   tz = tz, lat = lat, long = long)
 
+# estimate ecosystem metabolism using observed DO time series
+metab_obs <- ecometab(wtreg_res, DO_var = 'DO_obs', tz = tz,
+                      lat = lat, long = long)
 
+wins_in <- list(12, 6, 0.5)
+
+control <- list(factr = 1e7, parscale = c(5, 10, 1))
+lower <- c(0.1, 0.1, 0.1)
+upper <- c(30, 48, 4)
+ 
+strt <- Sys.time()
+
+fun_in <- function(wins_in){
+
+  txt <- unlist(wins_in)
+  print(Sys.time() - strt)
+  cat(txt, '\n')
+  
+  wtreg_res <- wtreg(dat, parallel = T, wins = wins_in, progress = T,
+                     tz = tz, lat = lat, long = long)
+  
+  metab_dtd <- ecometab(wtreg_res, DO_var = 'DO_nrm', tz = tz,
+                        lat = lat, long = long)
+  
+  out <- objfun(metab_obs, metab_dtd, vls = c('anomPg', 'anomRt'))
+  
+  cat(out, '\n\n')
+  
+  return(out)
+  
+}
+
+out <- optim(
+  wins_in, 
+  fun_in, 
+  method = 'L-BFGS-B', 
+  lower = lower, 
+  upper = upper, 
+  control = control
+)
+
+stopCluster(cl)
   
