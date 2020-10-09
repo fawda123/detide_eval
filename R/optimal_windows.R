9baf6dfbf7eec0b6b6b14ce340e5b387569ae05c
fawda123 <mbafs2012@gmail.com>
2020-10-09 14:50:26 -0400

    optimal windows loop through

--- R/optimal_windows.R
+++ R/optimal_windows.R
@@ -3,99 +3,116 @@ library(lubridate)
 library(WtRegDO)
 library(foreach)
 library(doParallel)
-# devtools::load_all('../WtRegDO/')
+
+# flnm <- 'PIERMO2017'
+# tz <- 'America/Jamaica'
+# lat <- 41.04
+# long <- -73.90
+# 
+# # data
+# dat <- read.csv(paste0('data/raw/', flnm, '.csv')) %>%
+#   mutate(DateTimeStamp = mdy_hm(DateTimeStamp, tz = tz)) %>%
+#   na.omit %>%
+#   unique
+# 
+# ncores <- detectCores()
+# cl <- makeCluster(ncores - 1)
+# registerDoParallel(cl)
+# 
+# out <- winopt(dat, tz = tz, lat = lat, long = long, wins = list(6, 6, 0.5), 
+#               control = list(factr = 1e+08, parscale = c(100, 200, 100), maxit = 100), parallel = T, vls = c('anomPg', 'anomRt'))
+# 
+
+optgrds <- tibble(
+  flnm = c('APNERR', 'APNERR2018', 'APNERR2020', 'HUDNERR', 'SAPDC', 'PIERMO', 'PIERMO2017'), 
+  tz = c('America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica'), 
+  lat = c(29.75, 29.75, 29.75, 42.017, 31.39, 41.04, 41.04), 
+  long = c(-85, -85, -85, -73.915, -81.28, -73.90, -73.90)
+  )
 
 ncores <- detectCores()  
-cl <- makeCluster(ncores)
-registerDoParallel(cl)
-
-flnm <- 'APNERR2020'
-tz <- 'America/Jamaica'
-lat <- 29.75
-long <- -85
-dy <- 12
-hr <- 6
-td <- 0.5
-
-# data
-dat <- read.csv(paste0('data/raw/', flnm, '.csv')) %>%
-  mutate(DateTimeStamp = mdy_hm(DateTimeStamp, tz = tz)) %>%
-  na.omit %>%
-  unique
-
-# weighted regression, optimal window widths for SAPDC from the paper
-wtreg_res <- wtreg(dat, wins = list(12, 6, 0.5), progress = T, parallel = T,
-                   tz = tz, lat = lat, long = long)
-
-# estimate ecosystem metabolism using observed DO time series
-metab_obs <- ecometab(wtreg_res, DO_var = 'DO_obs', tz = tz,
-                      lat = lat, long = long)
-
-wins_in <- list(12, 6, 0.5)
-
-control <- list(factr = 1e7, parscale = c(50, 100, 50))
-lower <- c(0.1, 0.1, 0.1)
-upper <- c(12, 12, 1)
- 
+registerDoParallel(cores = ncores - 1)
 strt <- Sys.time()
 
-fun_in <- function(wins_in){
+opts <- vector('list', length = nrow(optgrds))
+names(opts) <- optgrds$flnm
 
-  txt <- unlist(wins_in)
-  print(Sys.time() - strt)
-  cat(txt, '\n')
+for(i in 1:nrow(optgrds)){
   
-  wtreg_res <- wtreg(dat, parallel = T, wins = wins_in, progress = T,
-                     tz = tz, lat = lat, long = long)
+  flnm <- optgrds[[i, 'flnm']]
+  tz <- optgrds[[i, 'tz']]
+  lat <- optgrds[[i, 'lat']]
+  long <- optgrds[[i, 'long']]
+
+  cat(flnm, '\n\n')
   
-  metab_dtd <- ecometab(wtreg_res, DO_var = 'DO_nrm', tz = tz,
-                        lat = lat, long = long)
+  # data
+  dat <- read.csv(paste0('data/raw/', flnm, '.csv')) %>% 
+    mutate(DateTimeStamp = mdy_hm(DateTimeStamp, tz = tz)) %>% 
+    na.omit %>% 
+    unique
   
-  out <- objfun(metab_obs, metab_dtd, vls = c('anomPg', 'anomRt'))
+  out <- winopt(dat, tz = tz, lat = lat, long = long, wins = list(6, 6, 0.5), 
+                control = list(factr = 1e+07, parscale = c(100, 200, 100), maxit = 100), parallel = T, vls = c('anomPg', 'anomRt'))
   
-  cat(out, '\n\n')
-  
-  return(out)
+  opts[[flnm]] <- out
   
 }
 
-out <- optim(
-  wins_in, 
-  fun_in, 
-  method = 'L-BFGS-B', 
-  lower = lower, 
-  upper = upper, 
-  control = control
+# test when optim is done -------------------------------------------------
+
+optgrds <- tibble(
+  flnm = c('APNERR', 'APNERR2018', 'APNERR2020', 'HUDNERR', 'SAPDC', 'PIERMO', 'PIERMO2017'), 
+  tz = c('America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica', 'America/Jamaica'), 
+  lat = c(29.75, 29.75, 29.75, 42.017, 31.39, 41.04, 41.04), 
+  long = c(-85, -85, -85, -73.915, -81.28, -73.90, -73.90),
+  wins = list(
+    APNERR = list(4.45114875821087, 8.90461094963134, 0.876024796925915), 
+    APNERR2018 = list(6, 6, 0.5), 
+    APNERR2020 = list(6, 6, 0.5), 
+    HUDNERR = list(12, 0.1, 1), 
+    SAPDC = list(6, 6, 1), 
+    PIERMO = list(12, 12, 1), 
+    PIERMO2017 = list(12, 7.02594646999893, 1)
+    )
 )
 
-stopCluster(cl)
+cmps <- vector('list', length = nrow(optgrds))
+names(cmps) <- optgrds$flnm
+
+for(i in 1:nrow(optgrds)){
   
+  flnm <- optgrds[[i, 'flnm']]
+  tz <- optgrds[[i, 'tz']]
+  lat <- optgrds[[i, 'lat']]
+  long <- optgrds[[i, 'long']]
+  wins <- optgrds[[i, 'wins']][[1]]
+  
+  cat(flnm, '\n\n')
+  
+  # data
+  dat <- read.csv(paste0('data/raw/', flnm, '.csv')) %>% 
+    mutate(DateTimeStamp = mdy_hm(DateTimeStamp, tz = tz)) %>% 
+    na.omit %>% 
+    unique
 
-ncores <- detectCores()  
-cl <- makeCluster(ncores)
-registerDoParallel(cl)
+  wtreg_res <- wtreg(dat, wins = wins, progress = T, parallel = F,
+                     tz = tz, lat = lat, long = long)
 
-flnm <- 'APNERR2020'
-tz <- 'America/Jamaica'
-lat <- 29.75
-long <- -85
-dy <- 12
-hr <- 6
-td <- 0.5
 
-# data
-dat <- read.csv(paste0('data/raw/', flnm, '.csv')) %>%
-  mutate(DateTimeStamp = mdy_hm(DateTimeStamp, tz = tz)) %>%
-  na.omit %>%
-  unique
+  metab_obs <- ecometab(wtreg_res, DO_var = 'DO_obs', tz = tz,
+                        lat = lat, long = long)
+  metab_dtd <- ecometab(wtreg_res, DO_var = 'DO_nrm', tz = tz,
+                        lat = lat, long = long)
 
-wins <- list(12, 21.57143, 1.747619)
-wins <- list(13.86532, 20.6448, 1.6704)
-# weighted regression, optimal window widths for SAPDC from the paper
-wtreg_res <- wtreg(dat, wins = list(12, 12, 0.95), progress = T, parallel = T,
-                   tz = tz, lat = lat, long = long)
-
-metab_dtd <- ecometab(wtreg_res, DO_var = 'DO_nrm', tz = tz,
-                      lat = lat, long = long)
-
-meteval(metab_dtd, all = F)
+  obj <- objfun(metab_obs, metab_dtd, vl = c('anomPg', 'anomRt'))
+  
+  evalobs <- meteval(metab_obs, all = F)
+  evaldtd <- meteval(metab_dtd, all = F)
+ 
+  out <- data.frame(rbind(evalobs, evaldtd))
+  out$obj <- obj
+  
+  cmps[[flnm]] <- out
+  
+}
